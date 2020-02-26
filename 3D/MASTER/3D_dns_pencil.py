912a0992651ebd135ad41e32d7a7a8d1118f8886
danieloh <daniel.oe.halvorsen@gmail.com>
2020-02-26 14:37:20 +0100

    Cross product optimized with Numba

--- 3D/MASTER/3D_dns_pencil.py
+++ 3D/MASTER/3D_dns_pencil.py
@@ -2,92 +2,96 @@ from numpy import *
 from numpy.fft import fftfreq, fft, ifft, irfft2, fftn,fftshift,rfft,irfft
 from mpi4py import MPI
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import time
-from tqdm import tqdm
-
-from mpistuff.mpibase import work_arrays,work_array_dict
-
-
-# U is set to dtype float32
-# Reynoldsnumber determined by nu Re = 1600, nu = 1/1600
+from numba import jit, float32 as numfloat32
+try:
+    from tqdm import tqdm
+except ImportError:
+    pass
+from mpistuff.mpibase import work_arrays
 work_array = work_arrays()
+
+###############################################
+###############################################
+# USER CHOICE ##
 nu = 0.0000000625
-# nu = 0.00000625
-T = 40
+Tend = 40
 dt = 0.01
+N_tsteps = ceil(Tend/dt)
+bool_percentile = 0.01
+plotting = 'plot'
 L = 2*pi
-N = int(2 ** 6)
+N = int(2 ** 4)
+###############################################
+###############################################
+
 N_half = int(N / 2)
 N_nyquist=int(N/2+1)
 P1 = 1
+
+# Initialize MPI communication and set number of processes along each axis.
 comm = MPI.COMM_WORLD
 num_processes = comm.Get_size()
-mpitype = MPI.DOUBLE_COMPLEX
-
 if num_processes > 1:
+    ############
+    # USER CHOICE
+    # Make sure this number is smaller than nr of core available.
     P1 = 2
-
+    ############
+mpitype = MPI.DOUBLE_COMPLEX
 rank = comm.Get_rank()
-
 P2 = int(num_processes/P1)
 N1 = int(N/P1)
 N2 = int(N/P2)
-
-
 commxz = comm.Split(rank/P1)
 commxy = comm.Split(rank%P1)
-
 xzrank = commxz.Get_rank()
 xyrank = commxy.Get_rank()
 
-
+# Declaration of physical mesh
 x1 = slice(xzrank*N1,(xzrank+1)*N1,1)
 x2 = slice(xyrank*N2,(xyrank+1)*N2,1)
-X = mgrid[x1,x2,:N].astype(float)*L/N
-
+X = mgrid[x1,x2,:N].astype(float32)*L/N
+# Declaration of wave numbers (Fourier space)
 kx = fftfreq(N, 1. / N)
 kz = kx[:(N_half)].copy();
 kz[-1] *= -1
-
 k2 = slice(int(xyrank*N2),int((xyrank+1)*N2),1)
 k1 = slice(int(xzrank*N1/2),int(xzrank*N1/2 + N1/2),1)
-#k1 = slice(int(xzrank*N1/2),int((xzrank+1)*N1/2))
 K = array(meshgrid(kx[k2],kx,kx[k1],indexing='ij'),dtype=int)
 
-
+# Preallocate arrays, decomposed using a 2D-pencil approach
 U = empty((3, N1, N2, N), dtype=float32)
 U_hat = empty((3, N2, N, int(N_half/P1)), dtype=complex)
-P = empty((N1, N2, N))
+P = empty((N1, N2, N),dtype=float32)
 P_hat = empty((N2, N, int(N_half/P1)), dtype=complex)
 U_hat0 = empty((3, N2, N, int(N_half/P1)), dtype=complex)
 U_hat1 = empty((3, N2, N, int(N_half/P1)), dtype=complex)
-
 Uc_hat = empty((N, N2, int(N_half/P1)), dtype=complex)
 Uc_hatT = empty((N2, N, int(N_half/P1)), dtype=complex)
 U_mpi = empty((num_processes, N1, N2, N_half), dtype=complex)
-
 Uc_hat_x = empty((N, N2, int(N1/2)), dtype=complex)
 Uc_hat_y = empty((N2, N, int(N1/2)), dtype=complex)
 Uc_hat_z = empty((N1, N2, int(N_nyquist)), dtype=complex)
 Uc_hat_xr = empty((N, N2, int(N1/2)), dtype=complex)
-
-
 dU = empty((3, N2, N, int(N_half/P1)), dtype=complex)
-curl = empty((3, N1, N2, N))
+curl = empty((3, N1, N2, N),dtype=float32)
 
-#K = array(meshgrid(kx, kx[rank * Np:(rank + 1) * Np], kz, indexing="ij"), dtype=int)
+# Precompute wave number operators and dealias matrix.
 K2 = sum(K * K, 0, dtype=int)
-K_over_K2 = K.astype(float) / where(K2 == 0, 1, K2).astype(float)
+K_over_K2 = K.astype(float32) / where(K2 == 0, 1, K2).astype(float32)
 kmax_dealias = 2. / 3. * (N_half)
 dealias = array(
     (abs(K[0]) < kmax_dealias) * (abs(K[1]) < kmax_dealias) * (abs(K[2]) < kmax_dealias),
     dtype=bool)
 
+# Runge Kutta constants
 a = [1. / 6., 1. / 3., 1. / 3., 1. / 6.]
 b = [0.5, 0.5, 1.]
 #dir = '/home/danieloh/PycharmProjects/Project_Turbulence_Modelling/animation_folder/'
 
-def ifftn_mpi2(fu, u):
+def ifftn_mpi_slab(fu, u):
     # Inverse Fourier transform
     Uc_hat[:] = ifft(fu, axis=0)
     comm.Alltoall([Uc_hat, MPI.DOUBLE_COMPLEX], [U_mpi, MPI.DOUBLE_COMPLEX])
@@ -95,11 +99,13 @@ def ifftn_mpi2(fu, u):
     u[:] = irfft2(Uc_hatT, axes=(1, 2))
     return u
 
+
 def transform_Uc_zx(Uc_hat_z, Uc_hat_xr, P1):
     sz = Uc_hat_z.shape
     sx = Uc_hat_xr.shape
     Uc_hat_z[:, :, :-1] = rollaxis(Uc_hat_xr.reshape((P1, sz[0], sz[1], sx[2])), 0, 3).reshape((sz[0], sz[1], sz[2]-1))
     return Uc_hat_z
+
 
 def transform_Uc_xy(Uc_hat_x, Uc_hat_y, P):
     sy = Uc_hat_y.shape
@@ -214,11 +220,37 @@ def fftn_mpi(u, fu):
     return fu
 
 
-def Cross(a, b, c):
+def Cross_slow(a, b, c):
     # 3D cross product
+
     c[0] = fftn_mpi(a[1] * b[2] - a[2] * b[1], c[0])
     c[1] = fftn_mpi(a[2] * b[0] - a[0] * b[2], c[1])
     c[2] = fftn_mpi(a[0] * b[1] - a[1] * b[0], c[2])
+    return c
+
+@jit(nopython=True)
+def Cross(a,b,c):
+    for i in range(a.shape[1]):
+        for j in range(a.shape[2]):
+            for k in range(a.shape[3]):
+                a0 = a[0,i,j,k]
+                a1 = a[1,i,j,k]
+                a2 = a[2,i,j,k]
+                b0 = b[0,i,j,k]
+                b1 = b[1,i,j,k]
+                b2 = b[2,i,j,k]
+                c[0,i,j,k]=a1*b2-a2*b1
+                c[1,i,j,k]=a2*b0-a0*b2
+                c[2,i,j,k]=a0*b1-a1*b0
+    return c
+
+def Cross_T(c, a, b, work_array):
+    """c_k = F_k(a x b)"""
+    d = work_array[(a, 1, False)]
+    d = Cross(a,b,d)
+    c[0] = fftn_mpi(d[0], c[0])
+    c[1] = fftn_mpi(d[1], c[1])
+    c[2] = fftn_mpi(d[2], c[2])
     return c
 
 
@@ -236,7 +268,7 @@ def computeRHS(dU, rk):
         for i in range(3):
             U[i] = ifftn_mpi(U_hat[i], U[i])
     curl[:] = Curl(U_hat, curl)
-    dU = Cross(U, curl, dU)
+    dU = Cross_T(dU,U, curl,work_array)
     dU *= dealias
     P_hat[:] = sum(dU * K_over_K2, 0, out=P_hat)
     dU -= P_hat * K
@@ -354,22 +386,28 @@ def spectrum(length,u,v,w):
     fig.savefig(Figs_Path + Fig_file_name + '.pdf')
     plt.show()
 
-def reshapeGathered(u_gathered,N,N1,N2,P1,P2,num_processes):
+
+def reshape_loop(P1,P2,store_vector,u_gathered):
+    #TODO Optimize using Cython (or numba? )
+    counter = 0
+    for j in range(shape(store_vector)[1]):
+        for i in range(shape(store_vector)[0]):
+            store_vector[i][j] = u_gathered[counter]
+            counter = counter+1
+    return store_vector
+
+
+def reshapeGathered(u_gathered,N,N1,N2,P1,P2,num_processes,method):
     if num_processes == 1:
         u_reshaped = reshape(u_gathered, (3, N, N, N))
     else:
-        store_vector = empty(shape=(P1,P2)).tolist()
-        #row_Vectors = empty(shape=(P1)).tolist()
-        counter=0
-        for j in range(P2):
-            for i in range(P1):
+        if method=='concatenate':
+            store_vector = empty(shape=(P1, P2),dtype=float32).tolist()
+            store_vector = reshape_loop(P1,P2,store_vector,u_gathered)
 
-                store_vector[i][j]=u_gathered[counter]
-                counter +=1
-
-        first_concat = concatenate(store_vector,axis=2)
-        second_concat = concatenate(first_concat,axis=2)
-        u_reshaped = second_concat
+            first_concat = concatenate(store_vector,axis=2)
+            second_concat = concatenate(first_concat,axis=2)
+            u_reshaped = second_concat
     # new vector = concatenate((vec1,vec2,vec3,---),axis=..)
     # u_gathered has shape (N_process,3,N/N1,N/N2,N)
     # P1 = 2
@@ -387,31 +425,35 @@ U[0] = sin(X[0]) * cos(X[1]) * cos(X[2])
 U[1] = -cos(X[0]) * sin(X[1]) * cos(X[2])
 U[2] = 0
 
-#print('plot before transform')
-#plt.imshow(K2[0,:,:])
-#plt.imshow(U[0][:,:,int(N/2)],cmap='jet')
-#plt.show()
-
 if __name__ == '__main__':
-
 
     for i in range(3):
         U_hat[i] = fftn_mpi(U[i], U_hat[i])
 
-    # Time integral using a Runge Kutta scheme
     t = 0.0
     tstep = 0
-    mid_idx = int(N / 2)
-    pbar = tqdm(total=int(T / dt))
-    plotting = 'plot'
+    plot_step = N_tsteps*bool_percentile
     fig = plt.figure()
+    ims = []
+    mid_idx = int(N / 2)
+    try:
+        pbar = tqdm(total=int(Tend / dt))
+    except:
+        pass
+
+    '''
+    if rank==0:
+        progressfile = open('progressfile.txt','w+')
+        progressfile.truncate(0)
+    '''
+
     # ims is a list of lists, each row is a list of artists to draw in the
     # current frame; here we are just animating one artist, the image, in
     # each frame
-    ims = []
+
     
-    while t < T - 1e-8:
-    
+    while t < Tend - 1e-8:
+        # Time integral using a Runge Kutta scheme
         t += dt;
         U_hat1[:] = U_hat0[:] = U_hat
         for rk in range(4):
@@ -427,32 +469,32 @@ if __name__ == '__main__':
     
         if tstep%50==0:
             u_gathered = comm.gather(U, root=0)
-
             if rank==0:
-                u_reshaped = reshapeGathered(u_gathered,N,N1,N2,P1,P2,num_processes)
-
-
+                u_reshaped = reshapeGathered(u_gathered,N,N1,N2,P1,P2,num_processes,method='concatenate')
                 if plotting == 'animation':
-                    im = plt.imshow(U_test[0][:,:,int(N/2)],cmap='jet', animated=True)
+                    im = plt.imshow(u_reshaped[0][:,:,mid_idx],cmap='jet', animated=True)
                     ims.append([im])
                 if plotting == 'plot':
-                   # print('shape of gathered U',shape(u_plot))
-                   # print('shape of gathered U, concatenated',shape(U_test))
                     plt.imshow(u_reshaped[0][:,:,-1],cmap='jet')
                     plt.pause(0.05)
                     #plt.pause(0.05)
                 if plotting == 'spectrum':
-                    spectrum(N, U_test[0], U_test[1], U_test[2])
+                    spectrum(N, u_reshaped[0], u_reshaped[1], u_reshaped[2])
+                if plotting == 'savefig':
+                    plt.imshow(u_reshaped[0][:, :, -1], cmap='jet')
+                    plt.savefig('images/turb_t_'+str(int(tstep)))
+                if plotting=='noFigure':
+                    print('next iteration')
 
         tstep += 1
-        pbar.update(1)
-    
-    k = comm.reduce(0.5 * sum(U * U) * (1. / N) ** 3)
-    # if rank == 0:
-    #   assert round(k - 0.124953117517, 7) == 0
-    pbar.close()
-    
-    
+       # if rank ==0:
+            #progressfile.write("tstep= %d\r\n" % (tstep),flush=True)
+           # print('tstep= %d\r\n'%(tstep),flush=True)
+        try:
+            pbar.update(1)
+        except:
+            pass
+
     if plotting=='animation':
         if rank==0:
             ani = animation.ArtistAnimation(fig, ims, interval=2, blit=True,
